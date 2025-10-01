ccbc02be8be411ccc621d23ea366d2e520d3c3a2
Harshit Kant <Harshit.Kant@ecosoulhome.com>
2025-10-01 15:19:24 +0530

    fix dbConnection

--- Utils/dbConnection.js
+++ Utils/dbConnection.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
+  dialect: 'mysql',
   port: process.env.DB_PORT,
   dialectModule: tedious,
   dialectOptions: {
